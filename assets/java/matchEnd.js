d2faad192bccc69bf6649c6bdba81ee3829bf7fd
Ehren Davis <ehren.davis12@gmail.com>
2018-08-25 09:42:26 -0700

    add animations and timers on win-lose

--- assets/java/matchEnd.js
+++ assets/java/matchEnd.js
@@ -1,39 +1,44 @@
 
-function playerWinsMatch(){
-    /* alert("Player Wins"); */
+function playerWinsMatch() {
     $(displayWorld.mainMessage).text(messagesDictionary.playerWonMatch);
     $(displayWorld.mainMessage).css("color", "var(--battleWin)");
-        
-    $(displayEnemy.name).text("");
-    $(displayWorld.chosenEnemy).empty();
-    $(displayWorld.chosenEnemyAttack).empty();
-    $(displayWorld.chosenPlayerAttack).empty();
-    handlePlayerWinGame();
-    setGameUpToPlay();
+    revealBattleButtons(false);
+    $(displayWorld.chosenEnemy).toggle( "explode" );
+    setTimeout(function () {
+        $(displayEnemy.name).text("");
+        $(displayWorld.chosenEnemy).empty();
+        $(displayWorld.chosenEnemyAttack).empty();
+        $(displayWorld.chosenPlayerAttack).empty();
+        handlePlayerWinGame();
+        $(displayWorld.chosenEnemy).toggle( "explode" );
+        setGameUpToPlay();
+    }, 2000);
 }
 
-function enemyWinsMatch(){
-    /* alert("Enemy Wins"); */
+function enemyWinsMatch() {
     $(displayWorld.mainMessage).text(messagesDictionary.playerLostMatch);
     $(displayWorld.mainMessage).css("color", "var(--battleLose)");
+    revealBattleButtons(false);
     resetGame();
 }
 
-function handlePlayerWinGame(){
-    if ($(displayWorld.characterDiv).is(':empty')){
-        /* alert("Player Has won the GAME!!!"); */
+function handlePlayerWinGame() {
+    if ($(displayWorld.characterDiv).is(':empty')) {
         $(displayWorld.mainMessage).text(messagesDictionary.playerWonGame);
         $(displayWorld.mainMessage).css("color", "var(--battleWin)");
+        revealBattleButtons(false);
         resetGame();
     }
 }
 
-function resetGame(){
-    $(displayPlayer.name).text("");
-    $(displayWorld.chosenPlayer).empty();
-    $(displayWorld.chosenPlayerAttack).empty();
-    $(displayEnemy.name).text("");
-    $(displayWorld.chosenEnemy).empty();
-    $(displayWorld.chosenEnemyAttack).empty();
-    setGameUpToPlay();
+function resetGame() {
+    setTimeout(function () {
+        $(displayPlayer.name).text("");
+        $(displayWorld.chosenPlayer).empty();
+        $(displayWorld.chosenPlayerAttack).empty();
+        $(displayEnemy.name).text("");
+        $(displayWorld.chosenEnemy).empty();
+        $(displayWorld.chosenEnemyAttack).empty();
+        setGameUpToPlay();
+    }, 2000);
 }
